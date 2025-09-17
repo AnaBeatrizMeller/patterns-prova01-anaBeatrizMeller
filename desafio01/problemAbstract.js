2a9a53b8a953f0c30bfbce6cffd629641b4a5d76
Ana Beatriz Meller Mendes Silva <ana.292822@alunosatc.edu.br>
2025-09-17 19:27:38 -0300

    add exercicio Abstract

--- desafio01/problemAbstract.js
+++ desafio01/problemAbstract.js
@@ -1,1 +1,88 @@
-//TODO
+//Abstract Factory
+class LightButton {
+    render() {
+      return "Botão branco criado";
+    }
+  }
+  class DarkButton {
+    render() {
+      return "Botão preto criado";
+    }
+  }
+  
+  class LightWindow {
+    render() {
+      return "Janela clara aberta";
+    }
+  }
+  class DarkWindow {
+    render() {
+      return "Janela escura aberta";
+    }
+  }
+
+
+class ThemeFactory {
+    createDarkTheme() {
+        throw new Error("Método abstrato deve ser implementado");
+    }
+
+    createLightTheme() {
+        throw new Error("Método abstrato deve ser implementado");
+    }
+}
+
+class ButtonFactory extends ThemeFactory{ 
+    createDarkTheme() {
+        return new DarkButton
+    }
+
+    createLightTheme() {
+        return new LightButton
+    }
+}
+
+class WindowFactory extends ThemeFactory{ 
+    createDarkTheme() {
+        return new DarkWindow
+    }
+
+    createLightTheme() {
+        return new LightWindow
+    }
+}
+
+class StyleApp {
+    constructor(factory) {
+        this.factory = factory;
+    }
+
+    renderSystem() {
+        const dark = this.factory.createDarkTheme();
+        const light = this.factory.createLightTheme();
+
+        console.log(dark.render());
+        console.log(light.render())
+    }
+
+}
+
+function main() {
+    const factories = {
+        button: new ButtonFactory,
+        window: new WindowFactory
+    };
+
+    const types = ["button", "window"];
+
+    types.forEach((type) => {
+        console.log(`\n>> Fábrica selecionada: ${type} <<`);
+        const factory = factories[type];
+        if (!factory) throw new Error("Tipo de fábrica desconhecido");
+
+        const app = new StyleApp(factory);
+        app.renderSystem();
+    })
+}
+  
+main()
