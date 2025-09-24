81a8cc27b8e4dd2f6b3ef4b6f425949baccb8f1a
Ana Beatriz Meller Mendes Silva <ana.292822@alunosatc.edu.br>
2025-09-24 19:52:28 -0300

    testando pipeline

--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,86 @@
-console.log("Hello, World! 002");
+class Lanche {
+    constructor() {
+        this.pao = null;
+        this.carne = null;
+        this.queijo = null;
+        this.salada = null;
+        this.molho = null;
+    }
+  
+    show() {
+      console.log(`Lanche: 
+        pao: ${this.pao ? "Sim" : "Não"},
+        carne: ${this.carne ? "Sim" : "Não"},
+        queijo: ${this.queijo ? "Sim" : "Não"},
+        salada: ${this.salada ? "Sim" : "Não"},
+        molho: ${this.molho ? "Sim" : "Não"}
+      `);
+    }
+  }
+  
+
+class LancheBuilder {
+    constructor() {
+        this.lanche = new Lanche();
+    }
+
+    setPao(pao) {
+        this.lanche.pao = pao;
+        return this;
+    }
+
+    setCarne(carne) {
+        this.lanche.carne = carne;
+        return this;
+    }
+
+    setQueijo(queijo) {
+        this.lanche.queijo = queijo;
+        return this;
+    }
+
+    setSalada(salada) {
+        this.lanche.salada = salada;
+        return this;
+    }
+
+    setMolho(molho) {
+        this.lanche.molho = molho;
+        return this;
+    }
+    
+    build() {
+        return this.lanche;
+      }
+}
+
+class LancheDirector {
+    static buildCheeseburger() {
+        return new LancheBuilder()
+        .setPao(true)
+        .setCarne(true)
+        .setQueijo(true)
+        .setMolho(true)
+        .setSalada(false)
+        .build();
+    }
+
+    static buildLancheCompleto() {
+        return new LancheBuilder()
+        .setPao(true)
+        .setCarne(true)
+        .setQueijo(true)
+        .setMolho(true)
+        .setSalada(true)
+        .build();
+    }
+}
+
+
+
+const cheeseburger = LancheDirector.buildCheeseburger();
+const lancheCompleto = LancheDirector.buildLancheCompleto();
+
+
+cheeseburger.show();
+lancheCompleto.show();
